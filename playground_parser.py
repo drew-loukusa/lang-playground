5c01e215ef556252c2223c9f3a1b91e1f40c9baf
drew-loukusa <dlwerd@gmail.com>
2021-04-30 21:32:26 -0700

    Added AST code to playground parser to create AST.
    Added code to each parsing method in the PlaygroundParser.
    It now returns an AST from the 'program' function.
    Yet to be tested.

--- playground_parser.py
+++ playground_parser.py
@@ -1,3 +1,4 @@
+from abstract_syntax_tree import AST
 from token_def import PlaygroundTokens as PGT
 from playground_lexer import PlaygroundLexer
 from abstract_parser import AbstractParser, ParsingError
@@ -11,10 +12,11 @@ class PlaygroundParser(AbstractParser):
         self.testing = False
 
     def program(self): 
+        root = AST(artificial=True, name="$PROGRAM")
         try:
             while self.LA(1) in {PGT.LPAREN, PGT.NAME, PGT.NUMBER} or \
                 self.LT(1).text == 'print':
-                self.statement()
+                root.add_child(self.statement())
             
             if self.LA(1) != PGT.EOF:
                 raise ParsingError(f"Failed to reach EOF before parsing halted. Last token retrieved: {self.LT(1)} on line {self.input.line_number}")
@@ -27,80 +29,111 @@ class PlaygroundParser(AbstractParser):
 
     def statement(self):
 
+        root = None 
         # Parse built in print function 
         if self.LA(1) == PGT.NAME and self.LT(1).text == 'print':
-            self.pg_print()
+            root = self.pg_print()
 
         elif self.LA(1) == PGT.NAME and self.LA(2) == PGT.EQUAL:
-            self.assign()
+            root = self.assign()
 
         elif self.LA(1) in {PGT.LPAREN, PGT.NAME, PGT.NUMBER}:
-            self.add_expr()
+            root = self.add_expr()
             self.match(PGT.SEMI_COLON)
 
         else: 
             raise ParsingError(f"Expecting a statement; found {self.LT(1)} on line {self.input.line_number}")
+        
+        return root 
 
     def pg_print(self):
-        self.match(PGT.NAME)
+        root = self.match(PGT.NAME)
         self.match(PGT.LPAREN)
-        self.add_expr()
+        expr = self.add_expr()
         self.match(PGT.RPAREN)
         self.match(PGT.SEMI_COLON)
+
+        root.add_child(expr)
+        return root 
     
     def assign(self):
-        self.match(PGT.NAME)
-        self.match(PGT.EQUAL)
-        self.add_expr()
+        name = self.match(PGT.NAME)
+        root = self.match(PGT.EQUAL)
+        expr = self.add_expr()
         self.match(PGT.SEMI_COLON)
+
+        root.add_children(name, expr)
+        return root 
 
     def add_expr(self): 
         if self.LA(1) not in { PGT.LPAREN, PGT.NAME, PGT.NUMBER }:
             raise ParsingError(f"Expecting an add expression; found {self.LT(1)} on line {self.input.line_number}")
-        
-        self.mult_expr()
+        root = None 
+        left = self.mult_expr()
 
         while self.LA(1) in { PGT.PLUS, PGT.MINUS }:
-            self.add_op()
-            self.mult_expr()
+            if root != None:
+                left = root 
+            root = self.add_op()
+            right = self.mult_expr()
+            root.add_children(left, right)
+        
+        return root 
 
     def mult_expr(self):
         if self.LA(1) not in { PGT.LPAREN, PGT.NAME, PGT.NUMBER }:
             raise ParsingError(f"Expecting an add expression; found {self.LT(1)} on line {self.input.line_number}")
-
-        self.atom()
+        
+        root = None 
+        left = self.atom() 
 
         while self.LA(1) in { PGT.STAR, PGT.FSLASH }:
-            self.mult_op()
-            self.atom()
+            # Root can be set if we're parsing a continuous string of 
+            # multiplications:
+            # 2 * 2 * 2
+            # Tree looks like = * (2 2), 
+            #              then * ((* (2 2)) 2)
+            if root != None:
+                left = root 
+            root = self.mult_op()
+            right = self.atom()
+            root.add_children(left, right)
+
+        return root 
 
     def atom(self):
+        root = None 
         if self.LA(1) == PGT.NAME:
-            self.match(PGT.NAME)
+            root = self.match(PGT.NAME)
         elif self.LA(1) == PGT.NUMBER:
-            self.match(PGT.NUMBER)
+            root = self.match(PGT.NUMBER)
         elif self.LA(1) == PGT.LPAREN:
             self.match(PGT.LPAREN)
-            self.add_expr()
+            root = self.add_expr()
             self.match(PGT.RPAREN)
         else: 
             raise ParsingError(f"Expecting an atom; found {self.LT(1)} on line {self.input.line_number}")
+        return root 
 
     def add_op(self):
+        root = None 
         if self.LA(1) == PGT.PLUS:
-            self.match(PGT.PLUS)
+            root = self.match(PGT.PLUS)
         elif self.LA(1) == PGT.MINUS:
-            self.match(PGT.MINUS)
+            root = self.match(PGT.MINUS)
         else: 
             raise ParsingError(f"Expecting an add op ('+' or '-'); found {self.LT(1)} on line {self.input.line_number}")
+        return root 
 
     def mult_op(self):
+        root = None
         if self.LA(1) == PGT.STAR:
-            self.match(PGT.STAR)
+            root = self.match(PGT.STAR)
         elif self.LA(1) == PGT.FSLASH:
-            self.match(PGT.FSLASH)
+            root = self.match(PGT.FSLASH)
         else: 
             raise ParsingError(f"Expecting an mult op ('*' or '/'); found {self.LT(1)} on line {self.input.line_number}") 
+        return root 
 
 if __name__ == "__main__":
     # Sanity check, parser should parse all of this and raise no exceptions.
