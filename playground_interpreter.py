4aad9cf2a84bd5a39ba1080379bcac66a1766430
drew-loukusa <dlwerd@gmail.com>
2021-05-01 21:18:33 -0700

    Created interpreter file.

---
+++ playground_interpreter.py
@@ -0,0 +1,10 @@
+from base.abstract_syntax_tree import AST
+
+class PlaygroundInterpreter:
+
+    # GlobalScope globalscope ?
+    # MemorySpace globals = ???
+    # Don't need a stack for function memory, no functions yet
+    
+    def __init__(self):
+        self.root 
