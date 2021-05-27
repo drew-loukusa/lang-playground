6b737a7b62a698f05fb3f065195526e99a820b0e
drew-loukusa <dlwerd@gmail.com>
2021-05-27 11:32:50 -0700

    Make attrs, methods @properties of PG_Class.
    Use the @property decorator to generate properties 'attrs' and 'methods'
    for PG_Class from the inherited scope dict 'symbols'.
    
    It's easier to put all symbols in the 'symbols' dict and expose generated
    attributes to the user than to manage three seperate dicts.

--- playground/playground_scope.py
+++ playground/playground_scope.py
@@ -24,7 +24,40 @@ class PG_Class(PG_Scope):
         # Is this the "original" object? I.E. the class definition?
         self.is_class_def = is_class_def
         super().__init__(name=name)
-        self.methods = defaultdict(dict)
+    
+    def _is_func_dict(self, item):
+        return (
+            type(item) is dict 
+            and type(list(item.keys())[0]) is int 
+            and type(list(item.values())[0]) is PG_Function
+        )
+
+    @property
+    def attrs(self):
+        return { k:v for k,v in self.symbols.items() if not self._is_func_dict(v) }
+        
+    @property
+    def methods(self):
+        return { k:v for k,v in self.symbols.items() if self._is_func_dict(v) }
 
     def __repr__(self):
-        return f"<Class: {self.name}, attrs: {self.symbols} methods: {self.methods}>"
+        if self.is_class_def:
+            return f"<Class Definition: {self.name}, attrs: {self.attrs} >"
+        return f"<Instance of Class: {self.name}, attrs: {self.attrs} >"
+
+if __name__ == "__main__":
+    foo = PG_Class(name="FooClass")
+    foo.symbols['a'] = 10 
+    foo.symbols['b'] = 20
+    foo.symbols['func1'] = { 0: PG_Function(name="func1", params=None, code=None) }
+    foo.symbols['func2'] = { 1: PG_Function(name="func2", params=None, code=None) }
+
+    # for k,v in foo.symbols.items():
+    #     print(k,v)
+    print("ATTRS:")
+    print("===============")
+    print(foo.attrs)
+
+    print("\nMETHODS:")
+    print("===============")
+    print(foo.methods)
